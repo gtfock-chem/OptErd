06b2db5c72165dfee577f194dc2c4b838b08babf
Marat Dukhan <maratek@gmail.com>
2014-02-26 23:15:05 -0500

    Code clean-up in test programs and C interface

--- testprog/screening.c
+++ testprog/screening.c
@@ -8,114 +8,85 @@
 #include "screening.h"
 
 
-void schwartz_screening (BasisSet_t basis, int **shellptr,
-                         int **shellid, int **shellrid,
-                         double **shellvalue, int *nnz)
+void schwartz_screening(BasisSet_t basis, int **shellptrOut,
+    int **shellidOut, int **shellridOut,
+    double **shellvalueOut, int *nnzOut)
 {
     ERD_t erd;
-    int nshells;
-    int M;
-    int N;
-    int dimM;
-    int dimN;
-    int nints;
-    int iM;
-    int iN;
-    double mvalue;
-    double allmax;
-    int index;
-    double *integrals;
-    int *_shellptr;
-    double eta;
-    int _nnz;
-    double *_shellvalue;
-    int *_shellid;
-    int *_shellrid;
-    double *vpairs;
-    
-    CInt_createERD (basis, &erd, 1);
-    nshells = CInt_getNumShells (basis);
-    
-    vpairs = (double *)malloc (sizeof(double) * nshells * nshells);
-    assert (vpairs != NULL);
+    CInt_createERD(basis, &erd, 1);
+    const int nshells = CInt_getNumShells(basis);
 
-    allmax = 0.0;
-    for (M = 0; M < nshells; M++)
-    {
-        dimM = CInt_getShellDim (basis, M);
-        for (N = 0; N < nshells; N++)
-        {
-            dimN = CInt_getShellDim (basis, N);
-            CInt_computeShellQuartet (basis, erd, 0, M, N, M, N, &integrals, &nints);            
-            mvalue = 0.0;
-            if (nints != 0)
-            {
-                for (iM = 0; iM < dimM; iM++)
-                {
-                    for (iN = 0; iN < dimN; iN++)
-                    {
-                        index = iM * (dimN*dimM*dimN+dimN) + iN * (dimM*dimN+1);
-                        if (mvalue < fabs (integrals[index]))
-                        {
-                            mvalue = fabs (integrals[index]);                    
+    double *vpairs = (double *)malloc(sizeof(double) * nshells * nshells);
+    assert(vpairs != NULL);
+
+    double allmax = 0.0;
+    for (int M = 0; M < nshells; M++) {
+        const int dimM = CInt_getShellDim(basis, M);
+        for (int N = 0; N < nshells; N++) {
+            const int dimN = CInt_getShellDim(basis, N);
+
+            int nints;
+            double *integrals;
+            CInt_computeShellQuartet(basis, erd, 0, M, N, M, N, &integrals, &nints);
+
+            double mvalue = 0.0;
+            if (nints != 0) {
+                for (int iM = 0; iM < dimM; iM++) {
+                    for (int iN = 0; iN < dimN; iN++) {
+                        const int index = iM * (dimN*dimM*dimN+dimN) + iN * (dimM*dimN+1);
+                        if (mvalue < fabs(integrals[index])) {
+                            mvalue = fabs(integrals[index]);
                         }
                     }
                 }
             }
-            vpairs[M * nshells  + N] = mvalue;
-            if (mvalue > allmax)
-            {
+            vpairs[M * nshells + N] = mvalue;
+            if (mvalue > allmax) {
                 allmax = mvalue;
             }
         }
     }
 
     // init shellptr
-    _nnz = 0;
-    eta = TOLSRC*TOLSRC/allmax;
-    _shellptr = (int *)malloc (sizeof(int) * (nshells + 1));
-    assert (_shellptr != NULL);
-    memset (_shellptr, 0, sizeof(int) * (nshells + 1));
-    for (M = 0; M < nshells; M++)
-    {
-        for (N = 0; N < nshells; N++)
-        {
-            mvalue = vpairs[M * nshells  + N];
-            if (mvalue > eta)
-            {
-                _nnz++;
+    int nnz = 0;
+    const double eta = TOLSRC*TOLSRC / allmax;
+    int *shellptr = (int *)malloc(sizeof(int) * (nshells + 1));
+    assert(shellptr != NULL);
+    memset(shellptr, 0, sizeof(int) * (nshells + 1));
+    for (int M = 0; M < nshells; M++) {
+        for (int N = 0; N < nshells; N++) {
+            double mvalue = vpairs[M * nshells  + N];
+            if (mvalue > eta) {
+                nnz++;
             }
         }
-        _shellptr[M + 1] = _nnz;
+        shellptr[M + 1] = nnz;
     }
 
-    _shellvalue  = (double *)malloc (sizeof(double) * _nnz);
-    _shellid  = (int *)malloc (sizeof(int) * _nnz);
-    _shellrid  = (int *)malloc (sizeof(int) * _nnz);
-    assert (_shellvalue != NULL &&
-            _shellid != NULL &&
-             _shellrid != NULL);    
-    _nnz = 0;   
-    for (M = 0; M < nshells; M++)
-    {
-        for (N = 0; N < nshells; N++)
-        {
-            mvalue = vpairs[M * nshells  + N];
-            if (mvalue > eta)
-            {
-                _shellvalue[_nnz] = mvalue;                       
-                _shellid[_nnz] = N;
-                _shellrid[_nnz] = M;
-                _nnz++;
+    double *shellvalue  = (double *)malloc(sizeof(double) * nnz);
+    int *shellid  = (int *)malloc(sizeof(int) * nnz);
+    int *shellrid  = (int *)malloc(sizeof(int) * nnz);
+    assert((shellvalue != NULL) &&
+           (shellid != NULL) &&
+           (shellrid != NULL));
+    nnz = 0;
+    for (int M = 0; M < nshells; M++) {
+        for (int N = 0; N < nshells; N++) {
+            const double mvalue = vpairs[M * nshells  + N];
+            if (mvalue > eta) {
+                shellvalue[nnz] = mvalue;
+                shellid[nnz] = N;
+                shellrid[nnz] = M;
+                nnz++;
             }
         }
     }
-    (*nnz) = _nnz;
-    free (vpairs);
-    CInt_destroyERD (erd);
+    *nnzOut = nnz;
+    free(vpairs);
+    CInt_destroyERD(erd);
 
-    *shellid = _shellid;
-    *shellrid = _shellrid;
-    *shellptr = _shellptr;
-    *shellvalue = _shellvalue;
+    *shellidOut = shellid;
+    *shellridOut = shellrid;
+    *shellptrOut = shellptr;
+    *shellvalueOut = shellvalue;
 }
--- testprog/testCInt2.c
+++ testprog/testCInt2.c
@@ -22,15 +22,11 @@ static uint64_t get_cpu_frequency(void) {
 
 int main (int argc, char **argv)
 {
-    ERD_t erd;
-    int ns;
     int nnz;
     int *shellptr;
     int *shellid;
     int *shellrid;
     double *shellvalue;
-    double fraction;
-    
     if (argc != 5) {
         printf ("Usage: %s <basisset> <xyz> <fraction> <nthreads>\n", argv[0]);
         return -1;
@@ -38,104 +34,95 @@ int main (int argc, char **argv)
 
     const uint64_t freq = get_cpu_frequency();
 
-    fraction = atof (argv[3]);
-    assert (fraction > 0.0 && fraction <= 1.0);
+    const double fraction = atof (argv[3]);
+    assert(fraction > 0.0 && fraction <= 1.0);
     const int nthreads = atoi(argv[4]);
     #ifdef _OPENMP
-    omp_set_num_threads (nthreads);
+    omp_set_num_threads(nthreads);
     #else
     assert(nthreads == 1);
     #endif
     
     // load basis set
     BasisSet_t basis;
-    CInt_createBasisSet (&basis);
-    CInt_loadBasisSet (basis, argv[1], argv[2]);
-    schwartz_screening (basis, &shellptr, &shellid, &shellrid, &shellvalue, &nnz);
+    CInt_createBasisSet(&basis);
+    CInt_loadBasisSet(basis, argv[1], argv[2]);
+    schwartz_screening(basis, &shellptr, &shellid, &shellrid, &shellvalue, &nnz);
 
-    printf ("Molecule info:\n");
-    printf ("  #Atoms\t= %d\n", CInt_getNumAtoms (basis));
-    printf ("  #Shells\t= %d\n", CInt_getNumShells (basis));
-    printf ("  #Funcs\t= %d\n", CInt_getNumFuncs (basis));
-    printf ("  #OccOrb\t= %d\n", CInt_getNumOccOrb (basis));
-    printf ("  nthreads\t= %d\n", nthreads);
+    printf("Molecule info:\n");
+    printf("  #Atoms\t= %d\n", CInt_getNumAtoms(basis));
+    printf("  #Shells\t= %d\n", CInt_getNumShells(basis));
+    printf("  #Funcs\t= %d\n", CInt_getNumFuncs(basis));
+    printf("  #OccOrb\t= %d\n", CInt_getNumOccOrb(basis));
+    printf("  nthreads\t= %d\n", nthreads);
 
-    CInt_createERD (basis, &erd, nthreads);
+    ERD_t erd;
+    CInt_createERD(basis, &erd, nthreads);
 
     double* totalcalls = (double *) malloc (sizeof (double) * nthreads * 64);
-    assert (totalcalls != NULL);
+    assert(totalcalls != NULL);
     double* totalnintls = (double *) malloc (sizeof (double) * nthreads * 64);
-    assert (totalnintls != NULL);
+    assert(totalnintls != NULL);
     
     #pragma omp parallel for
     for (int i = 0; i < nthreads; i++) {
         totalcalls[i * 64] = 0.0;
         totalnintls[i * 64] = 0.0;
     }
-    printf ("Computing integrals ...\n");
+    printf("Computing integrals ...\n");
 
     // reset profiler
     erd_reset_profile ();
         
     //printf ("max memory footprint per thread = %lf KB\n",
     //    CInt_getMaxMemory (erd[0])/1024.0);
-    
-    ns = CInt_getNumShells (basis);
-    double timepass = 0.0;
 
-    int swap;
-    int tmpid;
-    int reallen;
-    
-    const int slen = shellptr[ns];
-    int* idx = (int *) malloc (sizeof (int) * slen);
-    assert (idx != NULL);
-    srand (1234);
-    for (int i = 0; i < slen; i++) {
-        idx[i] = i;
-    }
-    if (fraction != 1.0) {
-        for (int i = 0; i < slen - 1; i++) {
-            swap = (int)((double)rand()/RAND_MAX * (slen - i));
-            tmpid = idx[swap];
-            idx[swap] = idx[slen - i - 1];
-            idx[slen - i - 1] = tmpid;
-        }
-    }
-    reallen = (int)(slen * fraction);
-    reallen = ((reallen == 0) ? 1 : reallen);    
+    const int ns = CInt_getNumShells(basis);
+
+    srand(1234);
+    /* In (fraction) cases rand() returns value not greater than computationThreshold */
+    const int computationThreshold = lround(fraction * RAND_MAX);
+
     const uint64_t start_clock = __rdtsc(); 
     #pragma omp parallel
     {
         #ifdef _OPENMP
-        const int tid = omp_get_thread_num ();
+        const int tid = omp_get_thread_num();
         #else
         const int tid = 0;
         #endif
 
         #pragma omp for schedule(dynamic)
-        for (int k = 0; k < reallen; k++) {
-            const int i = idx[k];
+        for (int i = 0; i < shellptr[ns]; i++) {
             const int M = shellrid[i];
             const int N = shellid[i];
+            if (M > N)
+                continue;
+
             const double value1 = shellvalue[i];
-            for (int P = 0; P < ns; P++) {                               
+            for (int P = 0; P < ns; P++) {
                 const int start2 = shellptr[P];
                 const int end2 = shellptr[P + 1];
-                for (int j = start2; j < end2; j++) {                
+                for (int j = start2; j < end2; j++) {
                     const int Q = shellid[j];
-                    const double value2 = shellvalue[j];
-                    if (M > N || P > Q || (M + N) > (P + Q))
+                    if (P > Q)
                         continue;
+                    if ((M + N) > (P + Q))
+                        continue;
+
+                    const double value2 = shellvalue[j];
                     if (fabs(value1 * value2) < TOLSRC * TOLSRC)
                         continue;
 
-                    double *integrals;
-                    int nints;
-                    CInt_computeShellQuartet (basis, erd, tid, M, N, P, Q, &integrals, &nints);
+                    /* Sample random integer. With probability (fraction) process the shell quartet. */
+                    if (rand() <= computationThreshold) {
+                        double *integrals;
+                        int nints;
+                        CInt_computeShellQuartet(basis, erd, tid, M, N, P, Q, &integrals, &nints);
 
-                    totalcalls[tid * 64] += 1;
-                    totalnintls[tid * 64] += nints;
+                        totalcalls[tid * 64] += 1;
+                        totalnintls[tid * 64] += nints;
+                    }
                 }
             }
         }
@@ -147,30 +134,27 @@ int main (int argc, char **argv)
         totalnintls[0 * 64] = totalnintls[0 * 64] + totalnintls[i * 64];
     } 
     const uint64_t total_ticks = end_clock - start_clock;
-    timepass = ((double) total_ticks) / freq;
+    const double timepass = ((double) total_ticks) / freq;
 
-    printf ("Done\n");
-    printf ("\n");
-    printf ("Number of calls: %.6le, Number of integrals: %.6le\n",
-            totalcalls[0], totalnintls[0]);
-    printf ("Total GigaTicks: %.3lf, freq = %.3lf GHz\n",
-            (double) (total_ticks) * 1.0e-9, (double)freq/1.0e9);
-    printf ("Total time: %.4lf secs\n", timepass);
-    printf ("Average time per call: %.3le us\n",
-            1000.0 * 1000.0 * timepass / totalcalls[0]);
+    printf("Done\n");
+    printf("\n");
+    printf("Number of calls: %.6le, Number of integrals: %.6le\n", totalcalls[0], totalnintls[0]);
+    printf("Total GigaTicks: %.3lf, freq = %.3lf GHz\n", (double) (total_ticks) * 1.0e-9, (double)freq/1.0e9);
+    printf("Total time: %.4lf secs\n", timepass);
+    printf("Average time per call: %.3le us\n", 1000.0 * 1000.0 * timepass / totalcalls[0]);
 
     // use 1 if thread timing is not required
-    erd_print_profile (1);
+    erd_print_profile(1);
 
-    CInt_destroyERD (erd);
-    free (totalcalls);
-    free (totalnintls);
-    free (shellptr);
-    free (shellid);
-    free (shellvalue);
-    free (shellrid);
-    
-    CInt_destroyBasisSet (basis);
+    CInt_destroyERD(erd);
+    free(totalcalls);
+    free(totalnintls);
+    free(shellptr);
+    free(shellid);
+    free(shellvalue);
+    free(shellrid);
+
+    CInt_destroyBasisSet(basis);
 
     return 0;
 }
